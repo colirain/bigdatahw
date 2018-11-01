189bcea5d05ef20b694bcbfb4d1067b7f538bc45
colirain <904985646@qq.com>
2018-11-01 19:11:02 -0400

    2

---
+++ hw4/2/credentials.js
@@ -0,0 +1,6 @@
+module.exports={
+	"consumer_key": "L0wvzJIRsC4AgOCB9Mng4z5OP",
+	"consumer_secret": "MaSEsGLADbvQbPEUIINxF0IMx99GBDay6KV3xc5tqzvZ6G7U1X",
+	"access_token_key": "1052075882620301312-wvZyyn4PDLaPjzgwdvX8LQD7Nu6X1R",
+	"access_token_secret": "Reg1bUQJ7fsk2V3pnJ1qodqn1m17mDQYfSGYQ0GLyDX6I"
+}
