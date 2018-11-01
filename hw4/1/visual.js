e91e2a95351da418de4f1b90179b1f69e6711b79
colirain <904985646@qq.com>
2018-11-01 19:10:48 -0400

    1

--- hw4/1/visual.js
+++
@@ -1,5 +0,0 @@
-// <script src='https://rawgit.com/karpathy/tsnejs/master/tsne.jd'></script>
-// <script src='https://d3js.org/d3.v5.min.js'></script>
-//     // d3.csv("dataCluster.csv", function (src_data){
-//     // }
-    d3.select("body").append("p").text("new p")
